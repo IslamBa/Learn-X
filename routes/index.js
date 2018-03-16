5fcaf87118b5a2c7bcf65c408dc21200e8a38340
IslamBa <bajsujev563i@htl-ottakring.ac.at>
2018-03-16 11:51:06 +0100

    Login Seite erstellt und etwas bearbeitet

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/learnx', function(req, res, next) {
+  res.render('login');
+});
+
 module.exports = router;
