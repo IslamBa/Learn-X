32c0f5bbcfb851473408cefc6fd3ab80857bf3d5
IslamBa <bajsujev563i@htl-ottakring.ac.at>
2018-04-13 12:48:19 +0200

    Antworten Animation geändert zu slide Down/Up

--- public/javascripts/primary.js
+++ public/javascripts/primary.js
@@ -1,16 +1,13 @@
 var count = 0;
 
-$(".drop-icon").click(function(){
-     if(count == 0){
-    $(".drop-text").fadeIn();
+$(".drop-icon").click(function () {
+    if (count == 0) {
+        $(".drop-text").slideDown();
     }
 
-    if(count == 1)
-    {
-    $(".drop-text").fadeOut();
-    count = -1;
+    if (count == 1) {
+        $(".drop-text").slideUp();
+        count = -1;
     }
-
     count++;
-
 })
