d72c7367c901d0a8ccc7b77ed63e6158141ec3a9
dutt534 <dutt534v@htl-ottakring.ac.at>
2018-04-20 09:09:00 +0200

    erste schritte für den onePager

--- public/javascripts/homescript.js
+++ public/javascripts/homescript.js
@@ -35,3 +35,20 @@ $(".navLink").click(function () {
         $(this).append("Info");
     }
 })
+
+$(".main").onepage_scroll({
+    sectionContainer: "section",     
+    easing: "ease",                  
+                                     
+    animationTime: 1000,             
+    pagination: true,                
+    updateURL: false,                
+    beforeMove: function(index) {},  
+    afterMove: function(index) {},   
+    loop: false,                     
+    keyboard: true,                  
+    responsiveFallback: false,        
+                                     
+                                     
+    direction: "vertical"      
+ });
