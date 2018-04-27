29feb74ce928e778a6c9989ca33f6012bf5fb23e
IslamBa <bajsujev563i@htl-ottakring.ac.at>
2018-04-27 09:06:19 +0200

    login.js erstellt

--- model/datenbank.js
+++ model/datenbank.js
@@ -11,4 +11,12 @@ async function getUsers() {
     }
 }
 
-module.exports = { getUsers };
+async function addUser(obj) {
+    try {
+        await query("INSERT INTO benutzer (name, passwort) VALUES('$1', '$2')", [obj.name, obj.passwort]);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { getUsers, addUser };
