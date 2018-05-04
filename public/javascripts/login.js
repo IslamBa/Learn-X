655edd37f741c72b0ca145517bd607a0c330be03
IslamBa <bajsujev563i@htl-ottakring.ac.at>
2018-05-04 11:06:03 +0200

    Verbindung zur Datenbank hergestellt und name aus Datenbank in Index ausgegeben

---
+++ public/javascripts/login.js
@@ -0,0 +1,15 @@
+$("#btnReg").click(function(){
+    var name = $("#regName").val();
+    var passwort = $("#regPass").val();
+    $.ajax({
+        url: "/registrieren/neu",
+        method: "post",
+        data: { nam: name, pass: passwort},
+        success (){
+            console.log("neuen Benutzer hinzugefügt");
+        },
+        error(err){
+            console.log(err);
+        }
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    database: "learnx",
-    password: ""
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
+var datenbank = require("../model/datenbank");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  connection.query('SELECT * from benutzer', function (err, rows, fields) {
-    if (!err){
-        console.log('The solution is: ', rows);
-        var user = {b_id: rows[0].b_id, name: rows[0].name, passwort: rows[0].passwort};
-        console.log(user);
-        res.render('index', { title: 'Express', users : user });
-    }
-    else
-        console.log('Error while performing Query.');
+router.get('/', async function (req, res, next) {
+  res.render('index', { title: 'Express', users: await datenbank.getUsers() });
 });
 
-  
-});
-
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
-router.get('/registrieren', function(req, res, next) {
+router.get('/registrieren', function (req, res, next) {
   res.render('registrieren');
 });
 
-router.get('/home', function(req, res, next) {
+router.post('/registrieren/neu', function (req, res, next) {
+
+});
+
+router.get('/home', function (req, res, next) {
   res.render('home');
 });
 
-router.get('/fragen', function(req, res){
+router.get('/fragen', function (req, res) {
   res.render('fragen')
 });
 
-router.get('/impressum', function(req, res){
+router.get('/impressum', function (req, res) {
   res.render('impressum')
 });
 
