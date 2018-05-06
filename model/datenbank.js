21f08938b2400b837ae630c270dc9a4084f21c02
IslamBa <bajsujev563i@htl-ottakring.ac.at>
2018-05-06 20:49:27 +0200

    Merge Fehler ausgebessert beim registrieren und der datenbank.js datei

--- model/datenbank.js
+++ model/datenbank.js
@@ -14,13 +14,13 @@ connection.connect(function (err) {
 async function getUsers() {
     let promise = new Promise(resolve => {
         connection.query('SELECT * from benutzer', function (err, rows, fields) {
-            if (!err) {
-
-                
-                var user = { b_id: rows[i].b_id, name: rows[i].name, passwort: rows[i].passwort };
-                    
-                
-                resolve(user);
+            if (!err) {  
+                var users = [];
+                for(let el of rows){
+                    users.push({b_id: el.b_id, name: el.name, passwort: el.passwort})
+                }
+                //var user = { b_id: rows[0].b_id, name: rows[0].name, passwort: rows[0].passwort };
+                resolve(users);
             }
             else
                 console.log("Fehler beim Query aufgetreten");
