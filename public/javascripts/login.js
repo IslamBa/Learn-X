636a308ca7d8a49b65c765ffcdfb7e7b45de003c
IslamBa <bajsujev563i@htl-ottakring.ac.at>
2018-05-11 10:08:32 +0200

    login geändert

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -72,32 +72,6 @@ function Benutzer(name){
 }
 $(document).keypress(function(event) {
     if (event.keyCode == 13) {
-        let name = $("#name").val();
-        let passwort = $("#password").val();
-
-        $.ajax({
-            method: "post",
-            url: "/benutzer/" + name,
-            data: {
-                name: name,
-                passwort: passwort
-            },
-            success(res) {
-                if(res == true){
-                    window.location.href = "/home";
-                }
-                else if(res == false){
-                    alert("Benutzername und passwort stimmen nicht überein");
-                }
-                else{
-                    alert("Benutzername nicht bekannt");
-                }
-                
-            },
-            error(err){
-                console.log(err.responseText);
-                alert(err.responseText);
-            }
-        });
+        $(".login").click();
     }
 });
