58b9c60509305f36f2e7f591b2dbe61bd98846b5
tolgak54 <kordon363t@htl-ottakring.ac.at>
2018-05-11 09:45:13 +0200

    Login mit Enter

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -57,3 +57,35 @@ $(".login").click(function () {
         }
     });
 })
+
+$(document).keypress(function(event) {
+    if (event.keyCode == 13) {
+        let name = $("#name").val();
+        let passwort = $("#password").val();
+
+        $.ajax({
+            method: "post",
+            url: "/benutzer/" + name,
+            data: {
+                name: name,
+                passwort: passwort
+            },
+            success(res) {
+                if(res == true){
+                    window.location.href = "/home";
+                }
+                else if(res == false){
+                    alert("Benutzername und passwort stimmen nicht überein");
+                }
+                else{
+                    alert("Benutzername nicht bekannt");
+                }
+                
+            },
+            error(err){
+                console.log(err.responseText);
+                alert(err.responseText);
+            }
+        });
+    }
+});
